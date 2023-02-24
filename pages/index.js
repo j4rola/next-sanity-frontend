67be2d62f11c0eacf996f650767499e0e968a540
Joe Froula <jmfroula@gmail.com>
2023-02-24 11:35:53 -0800

    adds more queryoptions

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,21 @@ import { sanityClient, urlFor } from "../sanity"
 export default function Home({ pets }) {
 
   console.log(pets)
-  console.log(pets[0].image.asset._ref)
+  //console.log(pets[0].image.asset._ref)
   return (
-    <div className={styles.container}>
-      {pets.map(x =>  <><img style={{maxWidth: '300px'}} src={urlFor(x.image.asset._ref)}></img><h4>{x.name}</h4></>)}
+    <div className={styles.container}> 
+      {pets.map(x =>  <div style={{marginTop: '35px'}}><img style={{maxWidth: '300px'}} src={urlFor(x.image.asset._ref)}></img><h4>{x.name}</h4></div>)}
     </div>
   )
 }
 
 export const getServerSideProps = async () => {
-  const query = '*[ _type == "pet"]'
+
+  //get all pets and all owners
+  //const query = `*[ _type == "pet"]  {..., owner->}`
+
+  //get all pets that have Mary Jane as an owner 
+  const query = `*[ _type == "pet" && owner-> name == "Mary Jane"]`   
   const pets = await sanityClient.fetch(query)
 
   if (!pets.length) {
